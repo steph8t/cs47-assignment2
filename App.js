dcb66108c77dd01090435d5885af2ec17eca5958
ruslanjabari <rjabari@stanford.edu>
2022-12-31 19:20:31 -0800

    A2 is now less complex :)

--- App.js
+++ App.js
@@ -2,6 +2,36 @@ import AppLoading from 'expo-app-loading';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
 import { useFonts } from 'expo-font';
 import { Themes } from './assets/Themes';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Footer } from './app/components'
+
+/* This is the home screen used for the navigation system, we'll
+ * learn more about in the coming weeks! 
+ */
+function HomeScreen() {
+
+  /* insert your code here */
+
+  return (
+    <View style={styles.container}>
+    <Text
+      style={{
+        fontFamily: 'Sydney', // test to see if the font is loaded, feel free to remove this
+      }}>
+      Open up App.js to start working on your app!
+    </Text>
+    <Text
+      style={{
+        fontFamily: 'Sydney-Bold', // test to see if the font is loaded, feel free to remove this
+      }}>
+      ~Good luck~
+    </Text>
+  </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -13,23 +43,14 @@ export default function App() {
   StatusBar.setBarStyle(Themes.light.statusBar);
   /* ^Don't mind/edit this one either unless you decide to do the dark theme one, in that case, you will have to change it accordingly*/
 
-  /* insert your code here */
-
   return (
-    <View style={styles.container}>
-      <Text
-        style={{
-          fontFamily: 'Sydney', // test to see if the font is loaded, feel free to remove this
-        }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <Text
-        style={{
-          fontFamily: 'Sydney-Bold', // test to see if the font is loaded, feel free to remove this
-        }}>
-        ~Good luck~
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator tabBar={Footer} screenOptions={{
+        headerShown: false,
+      }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
